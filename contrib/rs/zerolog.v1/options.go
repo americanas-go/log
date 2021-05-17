958c27dad9fcf4f117e81f6a68af43f53d2f4d5b
cristiandanner <cristiandanner@gmail.com>
2021-05-17 10:19:52 -0300

    feat: add default options for zerolog

--- contrib/rs/zerolog.v1/options.go
+++ contrib/rs/zerolog.v1/options.go
@@ -1,27 +1,80 @@
 package zerolog
 
 type Options struct {
-	// formatter TEXT/JSON
-	Formatter string
+	Formatter string // formatter TEXT/JSON
 	Console   struct {
-		// enable/disable console logging
-		Enabled bool
-		// console log level
-		Level string
+		Enabled bool   // enable/disable console logging
+		Level   string // console log level
 	}
 	File struct {
-		Enabled bool
-		// enable/disable file logging
-		Level string
-		// log path
-		Path string
-		// log filename
-		Name string
-		// log file max size (MB)
-		MaxSize int
-		// enabled/disable file compress
-		Compress bool
-		// file max age
-		MaxAge int
+		Enabled  bool   // enable/disable file logging
+		Level    string // file log level
+		Path     string // file log path
+		Name     string // file log filename
+		MaxSize  int    // file log file max size (MB)
+		Compress bool   // enabled/disable file compress
+		MaxAge   int    // file max age
+	}
+}
+
+type Option func(options *Options)
+
+func WithFormatter(value string) Option {
+	return func(options *Options) {
+		options.Formatter = value
+	}
+}
+
+func WithConsoleEnabled(value bool) Option {
+	return func(options *Options) {
+		options.Console.Enabled = value
+	}
+}
+
+func WithConsoleLevel(value string) Option {
+	return func(options *Options) {
+		options.Console.Level = value
+	}
+}
+
+func WithFileEnabled(value bool) Option {
+	return func(options *Options) {
+		options.File.Enabled = value
+	}
+}
+
+func WithFileLevel(value string) Option {
+	return func(options *Options) {
+		options.File.Level = value
+	}
+}
+
+func WithFilePath(value string) Option {
+	return func(options *Options) {
+		options.File.Path = value
+	}
+}
+
+func WithFileName(value string) Option {
+	return func(options *Options) {
+		options.File.Name = value
+	}
+}
+
+func WithFileMaxSize(value int) Option {
+	return func(options *Options) {
+		options.File.MaxSize = value
+	}
+}
+
+func WithFileCompress(value bool) Option {
+	return func(options *Options) {
+		options.File.Compress = value
+	}
+}
+
+func WithFileMaxAge(value int) Option {
+	return func(options *Options) {
+		options.File.MaxAge = value
 	}
 }
