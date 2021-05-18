2a2aa8f98f87fbc8134d9d397dbcadec16d28205
Mauricio Lopes <mauricioalopes@gmail.com>
2021-05-18 14:57:22 -0300

    chore: removes unused func fieldsFromContext
    Refs #16

--- context.go
+++ context.go
@@ -2,28 +2,10 @@ package log
 
 import "context"
 
-// constant
-const key = "log_fields"
-
 func ToContext(ctx context.Context) context.Context {
 	return l.ToContext(ctx)
 }
 
 func FromContext(ctx context.Context) Logger {
 	return l.FromContext(ctx)
-}
-
-func fieldsFromContext(ctx context.Context) Fields {
-
-	var fields Fields
-
-	if ctx == nil {
-		return Fields{}
-	}
-
-	if param := ctx.Value(key); param != nil {
-		fields = ctx.Value(key).(Fields)
-	}
-
-	return fields
 }
