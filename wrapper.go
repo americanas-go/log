05f01a2a76f8777e23d4f8e02ce83ec47b172a7d
João Paulo Faria <jpfaria@gmail.com>
2022-07-14 11:46:41 -0300

    fix: removes sync.RWMutex from wrapper

--- wrapper.go
+++ wrapper.go
@@ -1,26 +1,17 @@
 package log
 
-import (
-	"sync"
-)
-
 // A global variable so that l functions can be directly accessed.
 var (
-	l  Logger = Noop{}
-	mu sync.RWMutex
+	l Logger = Noop{}
 )
 
 // NewLogger returns an instance of logger.
 // Deprecated: prefer SetGlobalLogger
 func NewLogger(logger Logger) {
-	mu.Lock()
-	defer mu.Unlock()
 	l = logger
 }
 
 func SetGlobalLogger(logger Logger) {
-	mu.Lock()
-	defer mu.Unlock()
 	l = logger
 }
 
@@ -28,9 +19,6 @@ func SetGlobalLogger(logger Logger) {
 //
 // For templating details see implementation doc.
 func Printf(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Printf(format, args...)
 }
 
@@ -38,17 +26,11 @@ func Printf(format string, args ...interface{}) {
 //
 // For templating details see implementation doc.
 func Tracef(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Tracef(format, args...)
 }
 
 // Trace logs a message at trace level.
 func Trace(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Trace(args...)
 }
 
@@ -56,17 +38,11 @@ func Trace(args ...interface{}) {
 //
 // For templating details see implementation doc.
 func Debugf(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Debugf(format, args...)
 }
 
 // Debug logs a message at debug level.
 func Debug(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Debug(args...)
 }
 
@@ -74,17 +50,11 @@ func Debug(args ...interface{}) {
 //
 // For templating details see implementation doc.
 func Infof(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Infof(format, args...)
 }
 
 // Info logs a message at info level.
 func Info(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Info(args...)
 }
 
@@ -92,17 +62,11 @@ func Info(args ...interface{}) {
 //
 // For templating details see implementation doc.
 func Warnf(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Warnf(format, args...)
 }
 
 // Warn logs a message at warn level.
 func Warn(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Warn(args...)
 }
 
@@ -110,88 +74,55 @@ func Warn(args ...interface{}) {
 //
 // For templating details see implementation doc.
 func Errorf(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Errorf(format, args...)
 }
 
 // Error logs a message at error level.
 func Error(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Error(args...)
 }
 
 // Panicf is equivalent to Printf() followed by a call to panic().
 func Panicf(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Panicf(format, args...)
 }
 
 // Panic is equivalent to Print() followed by a call to panic().
 func Panic(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Panic(args...)
 }
 
 // Fatal is equivalent to Print() followed by a call to os.Exit(1).
 func Fatal(args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Fatal(args...)
 }
 
 // Fatalf is equivalent to Printf() followed by a call to os.Exit(1).
 func Fatalf(format string, args ...interface{}) {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	l.Fatalf(format, args...)
 }
 
 // WithField adds a key and value to logger.
 func WithField(key string, value interface{}) Logger {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	return l.WithField(key, value)
 }
 
 // WithError adds an error as a field to logger
 func WithError(err error) Logger {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	return l.WithError(err)
 }
 
 // WithFields adds fields to logger.
 func WithFields(keyValues Fields) Logger {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	return l.WithFields(keyValues)
 }
 
 // WithTypeOf adds type information to logger.
 func WithTypeOf(obj interface{}) Logger {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	return l.WithTypeOf(obj)
 }
 
 // GetLogger returns instance of Logger.
 func GetLogger() Logger {
-	mu.RLock()
-	defer mu.RUnlock()
-
 	return l
 }
